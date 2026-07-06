2b83f4a9087f323dc041721d9c54f25c61dd60ec
Jill munanura <jillmunanura32@gmail.com>
2026-07-06 17:35:11 +0300

    Add report model for lost/found matching log

---
+++ lib/matching_logic.dart
@@ -0,0 +1,13 @@
+class Report {
+  final String category;
+  final String location;
+  final DateTime date;
+  final String description;
+
+  Report({
+    required this.category,
+    required this.location,
+    required this.date,
+    required this.description,
+  });
+}
