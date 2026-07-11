613592f4ba35540ea9555d06e0f50476eea3cc31
Atuheire Bonitah <atuheirebonitah9@gmail.com>
2026-07-11 20:50:01 +0300

    modified main.dart for authourization of a person

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'firebase_options.dart';
-import 'screens/report_item_screen.dart';
+import 'auth_gate.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -93,13 +93,13 @@ class FindItApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Find It',
+      title: 'FIND IT',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
-      home: const ReportItemScreen(),
+      home: const AuthGate(),
     );
   }
 }
